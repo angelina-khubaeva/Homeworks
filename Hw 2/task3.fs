1769e1712d4298fc0e027e7aff5c821da3865c7b
Angelina <angelina.khubaeva@mail.ru>
2014-04-06 20:04:50 +0400

    Hw 2, task 3

---
+++ Hw 2/task3.fs
@@ -0,0 +1,9 @@
+﻿let rec digitMult n = 
+    if n < 10 then n
+    else digitMult (n / 10) * (n % 10)
+
+printfn "Enter number:"
+
+let n = int(System.Console.ReadLine())
+
+printfn "%A" (digitMult n)
