785cd800534aa863154880b9d76ab01d520efa6f
Angelina <angelina.khubaeva@mail.ru>
2014-04-06 20:02:19 +0400

    Hw 2, task 1

---
+++ Hw 2/task1.fs
@@ -0,0 +1,8 @@
+﻿let rec reverseList list =
+    match list with
+    | [] -> list
+    | h :: t -> List.append (reverseList list.Tail) [list.Head]
+
+printfn "List: [1; 2; 3; 4; 5]"
+
+printfn "Result: %A" (reverseList [1; 2; 3; 4; 5])
