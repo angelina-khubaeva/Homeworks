99010469e53d691352399ff236ce43f276ff2c67
Angelina <angelina.khubaeva@mail.ru>
2014-04-06 20:02:06 +0400

    Hw 1, task 2

---
+++ Hw 1/task_2.fs
@@ -0,0 +1,16 @@
+﻿let rec fibonacci n =
+    match n with
+    | 1 | 2 -> 1
+    | _ -> fibonacci (n - 1) + fibonacci (n - 2)
+
+let rec printFib n  =
+    match n with 
+    | 1 -> printf "%d " (fibonacci (n))
+    | _ -> printFib (n - 1)
+           printf "%d " (fibonacci (n))
+
+printfn "Enter number:"
+
+let num = int(System.Console.ReadLine())
+           
+printFib(num)
