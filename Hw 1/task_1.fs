c4100e2e0652bd9e9cf4654d5ca49e883787cbee
Angelina <angelina.khubaeva@mail.ru>
2014-04-06 20:01:56 +0400

    Hw 1, task 1

---
+++ Hw 1/task_1.fs
@@ -0,0 +1,9 @@
+﻿let rec factorial num = 
+    if num = 0 then 1 
+    else num * factorial (num - 1)
+
+printfn "Enter number:"
+
+let n =  int(System.Console.ReadLine())
+
+printfn "n! = %A" (factorial n)
