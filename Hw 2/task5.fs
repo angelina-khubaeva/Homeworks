1b2d9db183c96f8580a9ffa4c1ae1b358bb3afd2
Angelina <angelina.khubaeva@mail.ru>
2014-04-06 20:05:10 +0400

    Hw 2, task 5

---
+++ Hw 2/task5.fs
@@ -0,0 +1,16 @@
+﻿let isPalindrome str = 
+    let StrLength = String.length str
+    let rec isPalindromeAssistant n length = 
+        if n >= length then printfn "It is a palindrome"
+        else
+            if str.[n] = str.[length] then isPalindromeAssistant (n + 1) (length - 1)
+            else printfn "It is not a palindrome"
+    isPalindromeAssistant 0 (StrLength - 1)
+
+printfn "Enter string:"
+
+let str = System.Console.ReadLine()
+
+printfn "\nResult: " 
+
+isPalindrome str
