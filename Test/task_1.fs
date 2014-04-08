52a656ceec64a202cc87873fb83a62ab56466f7d
Angelina <angelina.khubaeva@mail.ru>
2014-04-08 17:09:14 +0400

    test

---
+++ Test/task_1.fs
@@ -0,0 +1,11 @@
+﻿// [1, -1, 1, -1, ...]
+let seqInfinite1 = Seq.initInfinite (fun index ->
+    if index % 2 = 0 then 1 else -1)
+
+// [1, 2, 3, 4, ...]
+let seqInfinite2 = Seq.initInfinite (fun index -> index + 1)
+
+// [1, -2, 3, -4, ...] 
+let seqInfinite = Seq.map2 (fun x y -> x * y) seqInfinite1 seqInfinite2
+
+printfn "%A" seqInfinite
---
+++ Test/task_2.fs
@@ -0,0 +1,34 @@
+﻿// Список произведений n1 на все числа от n1 до n2 
+let rec fun2 n1 n2 =
+    if n2 = n1 then [n1 * n1]
+    else List.append (fun2 n1 (n2 - 1)) [n1 * n2]
+
+// Список всех произведений двух трехзначных чисел
+let rec fun1 n1 =
+    if n1 = 100 then fun2 100 999
+    else List.append (fun1 (n1 - 1)) (fun2 n1 999) 
+
+let isPalindrome str = 
+    let StrLength = String.length str
+    let rec isPalindromeAssistant n length = 
+        if n >= length then true
+        else
+            if str.[n] = str.[length] then isPalindromeAssistant (n + 1) (length - 1)
+            else false
+    isPalindromeAssistant 0 (StrLength - 1)
+    
+// Список всех палиндромов, полученных произведением двух трехзначных чисел
+let listOfPalindromes = List.filter (fun x -> isPalindrome (string x)) (fun1 999)
+
+// Выбор максимального числа из списка sumList
+let rec maxNumber (sumList: List<int>) max = 
+    if sumList.Length = 0 then max
+    elif sumList.Head >= max then 
+        let max1 = sumList.Head
+        maxNumber sumList.Tail max1
+        else maxNumber sumList.Tail max
+
+// Результат
+let num = maxNumber listOfPalindromes 999
+
+printfn "%A" num
