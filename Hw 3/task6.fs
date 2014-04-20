0dd6a959222797e15e93298e404895710491d948
Angelina <angelina.khubaeva@mail.ru>
2014-04-20 19:13:59 +0400

    Hw3, task6

---
+++ Hw 3/task6.fs
@@ -0,0 +1,17 @@
+﻿// #3.6
+// Функция, генерирующая бесконечную последовательность простых чисел.
+
+// isPrime возвращает true, если num - простое и false, если нет
+let isPrime num =
+    let mutable result: bool = true
+    if num = 2 then true
+    else
+    if num % 2 = 0 then result <- false
+    else 
+        for i = 3 to (num / 2) do
+            if num % i = 0 then result <- false
+    result
+
+let infinitSeqOfPrimes = Seq.filter (fun x -> isPrime x) (Seq.initInfinite (fun index -> index))
+
+printfn "%A" infinitSeqOfPrimes
