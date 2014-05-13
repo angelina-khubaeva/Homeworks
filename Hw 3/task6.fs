09767f75ab36d752d516147c9a1a949c986fb622
Angelina <angelina.khubaeva@mail.ru>
2014-05-13 20:18:00 +0400

    Without mutable variable

--- Hw 3/task6.fs
+++ Hw 3/task6.fs
@@ -3,14 +3,13 @@
 
 // isPrime возвращает true, если num - простое и false, если нет
 let isPrime num =
-    let mutable result: bool = true
+    let rec assist acc =
+        if acc > num / 2 then true
+        elif num % acc = 0 then false
+        else assist <| acc + 1
     if num = 2 then true
-    else
-    if num % 2 = 0 then result <- false
-    else 
-        for i = 3 to (num / 2) do
-            if num % i = 0 then result <- false
-    result
+    elif num % 2 = 0 then false
+    else assist 3
 
 let infinitSeqOfPrimes = Seq.filter (fun x -> isPrime x) (Seq.initInfinite (fun index -> index))
 
