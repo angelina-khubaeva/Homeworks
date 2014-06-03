8dd673b2de8bee5eb02f672b05578e80ebbbe08f
Angelina <angelina.khubaeva@mail.ru>
2014-06-03 23:23:53 +0400

    Hw 3, task 5

---
+++ Hw 3/task5.fs
@@ -0,0 +1,10 @@
+﻿type Expr =
+  | Sum of Expr * Expr
+  | Mult of Expr * Expr
+  | Num of int
+
+let rec countExpr expr =
+  match expr with
+    | Mult (x, y) -> countExpr(x) * countExpr(y)
+    | Sum (x, y) -> countExpr(x) + countExpr(y)
+    | Num x -> x
