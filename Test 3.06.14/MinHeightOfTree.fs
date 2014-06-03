39066a4d94a939c469e5ff495e5240c469d95f30
Angelina <angelina.khubaeva@mail.ru>
2014-06-03 17:23:19 +0400

    Test

---
+++ Test 3.06.14/MinHeightOfTree.fs
@@ -0,0 +1,35 @@
+﻿//Посчитать минимальное расстояние в дереве от корня до листьев.
+
+type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Tip
+    
+let rec height tree =
+    match tree with
+    | Tip _ -> 1
+    | Node(_, l, r) -> 1 + min (height l) (height r)
+
+let myTree1 = Node(0, Node(1, Node(2, Tip, Tip), Node(3, Tip, Tip)), Node(4, Tip, Tip))
+
+printfn "
+              Tree1
+
+                0          
+        1              4   
+   2         3      Tip Tip
+Tip Tip   Tip Tip          
+
+Result1: %A\n\n" (height myTree1)
+
+let myTree2 = Node(0, Node(1, Node(3, Tip, Tip), Node(4, Node(6, Node(7, Tip, Tip), Tip), Tip)), Node(2, Node(5, Tip, Tip), Tip))
+printfn "
+              Tree2
+
+                0             
+        1              2       
+   3         4              5    
+                   6              
+                        7             
+                     Tip Tip
+                       
+Result2: %A" (height myTree2)
---
+++ Test 3.06.14/Queue.fs
@@ -0,0 +1,36 @@
+﻿// Реализовать класс "Очередь". При попытке получить значение из пустой очереди должно бросаться исключение. Мутабельные переменные здесь разрешены.
+
+type public 'a List =
+    | Empty                             
+    | Node of 'a * 'a List              
+    member this.Reverse =               // Обращение списка
+        let rec reverse destList sourceList =
+            match sourceList with
+                 | Empty -> destList
+                 | Node(sourceHead, sourceTail) -> reverse (Node(sourceHead, destList)) sourceTail
+        reverse Empty this
+    member this.Add x = Node(x, this)   // Добавление элемента в список
+    member this.Head  =                 // Извлечение элемента из списка
+        match this with
+            | Empty -> failwith "Empty"
+            | Node(head, tail) -> head
+    member this.Tail = 
+        match this with
+            | Empty -> failwith "Empty"
+            | Node(head, tail) -> tail
+
+type 'a Queue (front:'a List, rear: 'a List)  =
+    let frontToBack = 
+        match front, rear with 
+        | Empty, rear -> (rear.Reverse, Empty) 
+        | _, _ -> (front, rear)
+    static member Empty = Queue(List.Empty, List.Empty)     // Пустая очередь - пара пустых списков
+    member this.Add value = Queue(front, rear.Add value)    // Добавление элемента в очередь
+    member this.Head = 
+        match frontToBack with
+            | Empty, _ -> failwith "Empty"
+            | List.Node(a, __), _ -> a
+    member this.Tail = 
+        match frontToBack with
+            |Empty, _ -> failwith "Empty"
+            |List.Node(a, tail), r -> Queue(tail, r)
